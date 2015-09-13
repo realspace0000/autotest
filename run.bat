2800504d33d21d556a7ba3844f3ed9203fe87253
realspece <akazawa@za2.so-net.ne.jp>
2015-09-13 14:44:23 +0900

    Revert "sample code committed."
    This reverts commit 34d1c916562a8264357a9d543a2fda5303a514a1.

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-%~d0
-cd %~p0
-set PYTHONPATH=%PYTHONPATH%;C:\workspace_python\autotest;C:\workspace_python\autotest\adbtest_services;C:\workspace_python\autotest\adbtest_testcase
-echo %PYTHONPATH%
-set RESULT_PATH=C:\workspace_python\autotest\work\result
-
-python adbtest_testcase\AdbTest1.py
